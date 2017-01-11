4709529132361477f5017bfe3d821d6066d4d967
Linus Pettersson <linus.pettersson@gmail.com>
2017-01-11 21:43:11 +0100

    Add solutions for day 6 part 1 and 2

---
+++ lib/day06/part01.ex
@@ -0,0 +1,25 @@
+defmodule AdventOfCode.Day06.Part01 do
+  def solve do
+    read_file()
+    |> Enum.map(&String.graphemes/1)
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&most_used/1)
+    |> Enum.join()
+  end
+
+  defp most_used(list) do
+    {char, _} =
+      list
+      |> Enum.group_by(&(&1))
+      |> Enum.max_by(fn({_, v})-> length(v) end)
+
+    char
+  end
+
+  defp read_file do
+    File.read!("lib/day06/input.txt")
+    |> String.trim
+    |> String.split("\n")
+  end
+end
