7a5b6a453858a668b489c9f025b3064dc0591ef7
Linus Pettersson <linus.pettersson@gmail.com>
2017-02-04 23:07:11 +0100

    Add solution for day 7 part 1

---
+++ lib/day07/part01.ex
@@ -0,0 +1,20 @@
+defmodule AdventOfCode.Day07.Part01 do
+  def solve do
+    read_file |> Enum.filter(&valid?/1) |> length()
+  end
+
+  defp valid?(ip), do: valid?(ip, :out, :no_abba)
+
+  defp valid?("", _, :abba), do: true
+  defp valid?("", _, :no_abba), do: false
+
+  defp valid?(<< a, b, b, a, rest :: binary >>, :out, _) when a != b, do: valid?(rest, :out, :abba)
+  defp valid?(<< a, b, b, a, _rest :: binary >>, :in, _) when a != b, do: false
+
+  defp valid?("[" <> rest, _, abba), do: valid?(rest, :in, abba)
+  defp valid?(<< _, _, _, "]", rest :: binary >>, :in, abba), do: valid?(rest, :out, abba)
+
+  defp valid?(<< _, rest :: binary >>, state, abba), do: valid?(rest, state, abba)
+
+  defp read_file, do: File.read!("lib/day07/input.txt") |> String.trim |> String.split("\n")
+end
